72a77ecce97acffa253f1fa6b37771a059606af7
MiUsuario <github@abelardolg.com>
2025-02-15 11:47:59 +0100

    One to one relantionship
    Ready to remove .gitkeep files.
    Ready to execute one to one relationship between User and Address entities.

--- src/main/java/HelloHibernateApp.java
+++ src/main/java/HelloHibernateApp.java
@@ -1,10 +1,14 @@
 import daos.StudentDAO;
+import daos.UserDAO;
+import entities.Address;
 import entities.Student;
+import entities.User;
 import utils.HibernateUtil;
 
 public class HelloHibernateApp {
     public static void main(String[] args) {
-        singleEntity();
+        //singleEntity();
+        oneToOne();
     }
 
     private static void singleEntity() {
@@ -17,6 +21,26 @@ public class HelloHibernateApp {
         );
 
         studentDAO.saveStudent(aStudent);
+
+        // Más operaciones ...
+
+        // Cerramos la sesión que se abrió en el DAO
+        HibernateUtil.shutdown();
+    }
+
+    private static void oneToOne() {
+        UserDAO userDAO = new UserDAO();
+
+        Address calleDecepcion = new Address();
+        calleDecepcion.setFullDescription("Calle de la decepción, 13");
+
+        User user = new User(
+                "NombreUsuario",
+                (byte) 18,
+                calleDecepcion
+        );
+
+        userDAO.saveUser(user);
 
         // Más operaciones ...
 
